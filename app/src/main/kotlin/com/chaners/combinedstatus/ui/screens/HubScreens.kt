2d765a11a38a755e428a1bc4a28e7aeb7253c09c
CHS-Haple <62180298+CHS-Haple@users.noreply.github.com>
2026-09-24 04:10:30 +0800

    ui: unify MIUIX page overscroll

--- app/src/main/kotlin/com/chaners/combinedstatus/ui/screens/HubScreens.kt
+++ app/src/main/kotlin/com/chaners/combinedstatus/ui/screens/HubScreens.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -28,6 +27,7 @@ import com.chaners.combinedstatus.ui.components.MiuixBlurredTopBar
 import com.chaners.combinedstatus.ui.components.rememberTopBarBackdrop
 import com.chaners.combinedstatus.ui.components.topBarBackdropSource
 import com.chaners.combinedstatus.ui.layout.pageContentPadding
+import com.chaners.combinedstatus.ui.layout.pageVerticalOverscroll
 import com.chaners.combinedstatus.ui.navigation.AppRoute
 import kotlinx.coroutines.launch
 import top.yukonga.miuix.kmp.basic.BasicComponent
@@ -222,7 +222,8 @@ private fun HubPage(
                 modifier =
                     Modifier
                         .fillMaxSize()
-                        .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        .pageVerticalOverscroll(scrollBehavior),
+                overscrollEffect = null,
                 contentPadding = pageContentPadding(
                     innerPadding = paddingValues,
                     outerBottomPadding = bottomContentPadding,
