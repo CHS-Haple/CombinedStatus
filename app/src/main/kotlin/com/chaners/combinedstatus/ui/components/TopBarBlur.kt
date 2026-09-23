f2bc70c8b55cee6b46dfc80647890752b326478b
CHS-Haple <62180298+CHS-Haple@users.noreply.github.com>
2026-09-24 03:17:41 +0800

    fix: use BoxScope matchParentSize

--- app/src/main/kotlin/com/chaners/combinedstatus/ui/components/TopBarBlur.kt
+++ app/src/main/kotlin/com/chaners/combinedstatus/ui/components/TopBarBlur.kt
@@ -1,7 +1,6 @@
 package com.chaners.combinedstatus.ui.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
