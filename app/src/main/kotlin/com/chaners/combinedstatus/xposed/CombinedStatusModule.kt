221b1948ddff06c84907d86a3bc8c813a2d539b1
CHS-Haple <62180298+CHS-Haple@users.noreply.github.com>
2026-09-25 18:05:13 +0800

    fix: wire native battery hide to island occupancy

--- app/src/main/kotlin/com/chaners/combinedstatus/xposed/CombinedStatusModule.kt
+++ app/src/main/kotlin/com/chaners/combinedstatus/xposed/CombinedStatusModule.kt
@@ -658,6 +658,8 @@ class CombinedStatusModule : XposedModule() {
                             log(Log.INFO, TAG, event)
                         }
                     },
+                    onNativeHideChanged =
+                        SystemUiNativeCombinedParticipantOwner::onNativeBatteryHideChanged,
                 )
         ) {
             SystemUiNativeBatterySuppressionOwner.InstallResult.Installed,
